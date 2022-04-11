e27d2908d2faebbc616b505c4105871e035d6227
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-12 04:38:04 +0700

    feature/initial commit ✨✨

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <React.Fragment>
+            <h1>React Visualization</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, magni!</p>
+        </React.Fragment>
+    )
+}
+
+export default Homepage
